b72be0e1f0ac0264897418e5c573ecc1e0e0dd6f
Beabfekad Zikie <92110044+beabzk@users.noreply.github.com>
2023-10-06 23:31:40 +0300

    Implimented Chapter Selector

--- fetch.js
+++ fetch.js
@@ -1,6 +1,7 @@
-const selectElement = document.querySelector('#chapSelector');
+const selectElement = document.querySelector('#bookSelector');
 const bookNameElement = document.querySelector('#bookName');
 const contentPageElement = document.querySelector('#contentPage');
+const chapterSelectElement = document.querySelector('#chapSelector');
 
 const bookNameToFile = {
 
@@ -84,23 +85,62 @@ fetch('Bible-kjv/Books.json')
     });
   });
 
-selectElement.addEventListener('change', (event) => {
-  const selectedBook = event.target.value;
-  bookNameElement.textContent = selectedBook;
+// Define a function to update the displayed book
+function updateBook(selectedBook) {
+  // bookNameElement.textContent = selectedBook;
   const fileName = bookNameToFile[selectedBook];
   fetch(`Bible-kjv/${fileName}.json`)
     .then(response => response.json())
     .then(jsonBook => {
+      // Clear any existing options in the chapter selector
       contentPageElement.innerHTML = '';
-      jsonBook.chapters.forEach(chapter => {
-        const chapterElement = document.createElement('h2');
-        chapterElement.textContent = `Chapter ${chapter.chapter}`;
-        contentPageElement.appendChild(chapterElement);
-        chapter.verses.forEach(verse => {
-          const verseElement = document.createElement('p');
-          verseElement.textContent = `${verse.verse}. ${verse.text}`;
-          contentPageElement.appendChild(verseElement);
-        });
+
+      // Populate the chapter selector with options
+      jsonBook.chapters.forEach((chapter, index) => {
+        const optionElement = document.createElement('option');
+        optionElement.value = index + 1; // chapter numbers start at 1
+        optionElement.textContent = `Chapter ${index + 1}`;
+        chapterSelectElement.appendChild(optionElement);
       });
+
+      // Reset the chapter selector to the first option
+      chapterSelectElement.selectedIndex = 0;
+
+      // Display the first chapter by default
+      updateChapter(jsonBook, 0);
+    });
+}
+
+// Define a function to update the displayed chapter
+function updateChapter(jsonBook, chapterIndex) {
+  const chapter = jsonBook.chapters[chapterIndex];
+  // Update the book name and clear any existing content
+  bookNameElement.textContent = `${jsonBook.book} ${chapter.chapter}`;
+  contentPageElement.innerHTML = '';
+  // Display the verses of the selected chapter
+  chapter.verses.forEach(verse => {
+    const verseElement = document.createElement('p');
+    verseElement.textContent = `${verse.verse}. ${verse.text}`;
+    contentPageElement.appendChild(verseElement);
+  });
+}
+
+// Add event listener to select element
+selectElement.addEventListener('change', (event) => {
+  updateBook(event.target.value);
+});
+// Add event listener to chapter selector
+chapterSelectElement.addEventListener('change', (event) => {
+  const selectedBook = selectElement.value;
+  const fileName = bookNameToFile[selectedBook];
+  fetch(`Bible-kjv/${fileName}.json`)
+    .then(response => response.json())
+    .then(jsonBook => {
+      updateChapter(jsonBook, event.target.value - 1);
     });
 });
+
+// Call updateBook for 'Genesis' when the page loads
+window.onload = function() {
+  updateBook('Genesis');
+}
