34e77d9aec3f27683cd75c7ae66eba5330c03069
Beabfekad Zikie <92110044+beabzk@users.noreply.github.com>
2023-10-16 16:32:01 +0300

    File Renaming and Reorganization

--- src/scripts/fetchBooks.js
+++ src/scripts/fetchBooks.js
@@ -1,77 +1,10 @@
+import { BOOK_NAME_MAP } from './bookNames.js';
+
 const selectElement = document.querySelector('#bookSelector');
 const bookNameElement = document.querySelector('#bookName');
 const contentPageElement = document.querySelector('#contentPage');
 const chapterSelectElement = document.querySelector('#chapSelector');
 
-const bookNameToFile = {
-
-  "1 Chronicles": "1Chronicles",
-  "1 Corinthians": "1Corinthians",
-  "1 John": "1John",
-  "1 Kings": "1Kings",
-  "1 Peter": "1Peter",
-  "1 Samuel": "1Samuel",
-  "1 Thessalonians": "1Thessalonians",
-  "1 Timothy": "1Timothy",
-  "2 Chronicles": "2Chronicles",
-  "2 Corinthians": "2Corinthians",
-  "2 John": "2John",
-  "2 Kings": "2Kings",
-  "2 Peter": "2Peter",
-  "2 Samuel": "2Samuel",
-  "2 Thessalonians": "2Thessalonians",
-  "2 Timothy": "2Timothy",
-  "3 John": "3John",
-  "Acts": "Acts",
-  "Amos": "Amos",
-  "Colossians": "Colossians",
-  "Daniel": "Daniel",
-  "Deuteronomy": "Deuteronomy",
-  "Ecclesiastes": "Ecclesiastes",
-  "Ephesians": "Ephesians",
-  "Esther": "Esther",
-  "Exodus": "Exodus",
-  "Ezekiel": "Ezekiel",
-  "Ezra": "Ezra",
-  "Galatians": "Galatians",
-  "Genesis": "Genesis",
-  "Habakkuk": "Habakkuk",
-  "Haggai": "Haggai",
-  "Hebrews": "Hebrews",
-  "Hosea": "Hosea",
-  "Isaiah": "Isaiah",
-  "James": "James",
-  "Jeremiah": "Jeremiah",
-  "Job": "Job",
-  "Joel": "Joel",
-  "John": "John",
-  "Jonah": "Jonah",
-  "Joshua": "Joshua",
-  "Jude": "Jude",
-  "Judges": "Judges",
-  "Lamentations": "Lamentations",
-  "Leviticus": "Leviticus",
-  "Luke": "Luke",
-  "Malachi": "Malachi",
-  "Mark": "Mark",
-  "Matthew": "Matthew",
-  "Micah": "Micah",
-  "Nahum": "Nahum",
-  "Nehemiah": "Nehemiah",
-  "Numbers": "Numbers",
-  "Obadiah": "Obadiah",
-  "Philemon": "Philemon",
-  "Philippians": "Philippians",
-  "Proverbs": "Proverbs",
-  "Psalms": "Psalms",
-  "Revelation": "Revelation",
-  "Romans": "Romans",
-  "Ruth": "Ruth",
-  "Song of Solomon": "SongofSolomon",
-  "Titus": "Titus",
-  "Zechariah": "Zechariah",
-  "Zephaniah": "Zephaniah"
-};
 
 // fetches the names of the books
 fetch('../Bible-kjv/Books.json')
@@ -88,7 +21,7 @@ fetch('../Bible-kjv/Books.json')
 // Define a function to update the displayed book
 function updateBook(selectedBook) {
   // bookNameElement.textContent = selectedBook;
-  const fileName = bookNameToFile[selectedBook];
+  const fileName = BOOK_NAME_MAP[selectedBook];
   fetch(`../Bible-kjv/${fileName}.json`)
     .then(response => response.json())
     .then(jsonBook => {
@@ -132,7 +65,7 @@ selectElement.addEventListener('change', (event) => {
 // Add event listener to chapter selector
 chapterSelectElement.addEventListener('change', (event) => {
   const selectedBook = selectElement.value;
-  const fileName = bookNameToFile[selectedBook];
+  const fileName = BOOK_NAME_MAP[selectedBook];
   fetch(`../Bible-kjv/${fileName}.json`)
     .then(response => response.json())
     .then(jsonBook => {
