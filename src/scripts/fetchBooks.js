477269aa970e583f0828378ce62aaa209e6cd5c6
Beabfekad Zikie <92110044+beabzk@users.noreply.github.com>
2023-10-16 19:53:42 +0300

    Fix chapter selector not clearing (fixes #3)

--- src/scripts/fetchBooks.js
+++ src/scripts/fetchBooks.js
@@ -1,6 +1,6 @@
 import { BOOK_NAME_MAP } from './bookNames.js';
 
-const selectElement = document.querySelector('#bookSelector');
+const bookSelectElement = document.querySelector('#bookSelector');
 const bookNameElement = document.querySelector('#bookName');
 const contentPageElement = document.querySelector('#contentPage');
 const chapterSelectElement = document.querySelector('#chapSelector');
@@ -14,19 +14,24 @@ fetch('../Bible-kjv/Books.json')
       const optionElement = document.createElement('option');
       optionElement.value = book;
       optionElement.textContent = book;
-      selectElement.appendChild(optionElement);
+      bookSelectElement.appendChild(optionElement);
     });
   });
 
-// Define a function to update the displayed book
 function updateBook(selectedBook) {
-  // bookNameElement.textContent = selectedBook;
-  const fileName = BOOK_NAME_MAP[selectedBook];
-  fetch(`../Bible-kjv/${fileName}.json`)
+
+  // Clear existing options
+  chapterSelectElement.innerHTML = ''; 
+  contentPageElement.innerHTML = '';
+  // Reset the chapter selector to the first option
+  chapterSelectElement.selectedIndex = 0;
+
+  const bookFile = BOOK_NAME_MAP[selectedBook];
+  fetch(`../Bible-kjv/${bookFile}.json`)
     .then(response => response.json())
     .then(jsonBook => {
-      // Clear any existing options in the chapter selector
-      contentPageElement.innerHTML = '';
+
+
 
       // Populate the chapter selector with options
       jsonBook.chapters.forEach((chapter, index) => {
@@ -36,15 +41,14 @@ function updateBook(selectedBook) {
         chapterSelectElement.appendChild(optionElement);
       });
 
-      // Reset the chapter selector to the first option
-      chapterSelectElement.selectedIndex = 0;
+
 
       // Display the first chapter by default
       updateChapter(jsonBook, 0);
     });
 }
 
-// Define a function to update the displayed chapter
+// A function to update the displayed chapter
 function updateChapter(jsonBook, chapterIndex) {
   const chapter = jsonBook.chapters[chapterIndex];
   // Update the book name and clear any existing content
@@ -59,14 +63,16 @@ function updateChapter(jsonBook, chapterIndex) {
 }
 
 // Add event listener to select element
-selectElement.addEventListener('change', (event) => {
+bookSelectElement.addEventListener('change', (event) => {
   updateBook(event.target.value);
 });
 // Add event listener to chapter selector
 chapterSelectElement.addEventListener('change', (event) => {
-  const selectedBook = selectElement.value;
-  const fileName = BOOK_NAME_MAP[selectedBook];
-  fetch(`../Bible-kjv/${fileName}.json`)
+
+  const selectedBook = bookSelectElement.value;
+  const bookFile = BOOK_NAME_MAP[selectedBook];
+
+  fetch(`../Bible-kjv/${bookFile}.json`)
     .then(response => response.json())
     .then(jsonBook => {
       updateChapter(jsonBook, event.target.value - 1);
